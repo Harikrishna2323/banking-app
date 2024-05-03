749d735e0ad929dde63f152d6666015cd76e2718
Harikrishna2323 <harikb23@gmail.com>
2024-05-03 22:46:38 +0530

    first commit

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,9 @@
+import { Inter, IBM_Plex_Serif } from "next/font/google";
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return <main></main>;
+}
